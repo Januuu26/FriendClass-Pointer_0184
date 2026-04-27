c0d57de27fa078b4c4bb1d5c1184f3933e95f7e3
Januuu26 <b77286116@gmail.com>
2026-04-27 16:08:33 +0700

    membuat otput

--- Friendfunctionsatu.cpp
+++ Friendfunctionsatu.cpp
@@ -23,3 +23,15 @@ public:
     void displayALL(orang &a);
 };
 
+void siswa::displayALL(orang &a)
+{
+    cout << id  << endl << a.nama;
+}   
+void orang::setnama(string pnama)
+{
+    nama = pnama;
+}
+void siswa::setid(int pid)
+{
+    id = pid;
+}
